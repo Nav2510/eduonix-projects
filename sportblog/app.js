ccb025b0fc5e35aedf1ae08234b3df843a1be80f
Nav2510 <singh.navdeep2510@gmail.com>
2019-05-15 00:11:56 +0530

    Added the sportblog project, express-messages middleware issue is their

---
+++ sportblog/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressValidator = require('express-validator');
+
+//Port
+const port = 3000;
+
+//init app
+const app = express();
+
+//View setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+//Setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// //Express messages
+// app.use(require('connect-flash'));
+// app.use((req, res, next) => {
+//   res.locals.messages = require('express-messages')(req, res);
+//   next();
+// });
+
+//Express validator
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+    const namespace = param.split('.'),
+    root = namespace.shift(),
+    formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}));
+
+app.get('/', (req, res, next) => {
+  res.send('HELLLOW');
+});
+
+app.listen(port, () => {
+  console.log('Server started on port : '+port);
+});
